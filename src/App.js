b6bc556ea6615a06351a033e7637558f7c67f1e4
Hiro <hiromichi.s.617@gmail.com>
2021-01-20 10:33:31 +0900

    CSS>SCSS

--- src/App.js
+++ src/App.js
@@ -1,50 +1,17 @@
 import Vegetables from "./components/Vegetables";
 import AnimalList from "./components/AnimalList";
 import { useState } from "react";
+import "../src/App.css";
 
 const App = () => {
   const [tab, setTab] = useState("animalList");
-  const center = {
-    textAlign: "center",
-  };
-  const m = {
-    margin: "32px 0 0 0",
-  };
-  const btn1 = {
-    height: "48px",
-    backgroundColor: "#fff",
-    borderRight: "1px solid",
-    borderTop: "1px solid",
-    borderLeft: "1px solid",
-    marginBottom: "-1px",
-    borderBottom: "1px solid",
-    ":active,:focus": {
-      borderBottom: "transparent",
-      backgroundColor: "pink",
-      outLine: "none",
-    },
-  };
-  const btn2 = {
-    height: "48px",
-    backgroundColor: "#fff",
-    borderRight: "1px solid",
-    borderTop: "1px solid",
-    borderLeft: "transparent",
-    marginBottom: "-1px",
-    borderBottom: "1px solid",
-    ":active,:focus": {
-      borderBottom: "transparent",
-      backgroundColor: "pink",
-      outLine: "none",
-    },
-  };
   return (
-    <main style={center}>
-      <div className="button" style={m}>
-        <button style={btn1} onClick={() => setTab("animalList")}>
+    <main className="center">
+      <div className="button m">
+        <button className="btn1" onClick={() => setTab("animalList")}>
           動物一覧
         </button>
-        <button style={btn2} onClick={() => setTab("vegetables")}>
+        <button className="btn2" onClick={() => setTab("vegetables")}>
           野菜一覧
         </button>
       </div>
